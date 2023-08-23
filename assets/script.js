0774824d5681b2090c82acbf7ae8e8fa3e7c8719
Taylor <taysully94@gmail.com>
2023-08-23 17:51:51 -0400

    adds password variables, a math generating function unfinished

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,34 @@
 // Assignment code here
 
 
+//button click - starts the prompts 
+
+//asks password length (I choose a length of at least 8 characters and no more than 128 characters)
+
+//asks character type preferences (I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters)
+
+//input should be validated and at least one character type should be selected
+
+//generates password and puts it in either prompt or on the screen
+
+var promptIssues 
+
+
+function generatePassword() {
+  var numbers = "0123456789";
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var UpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var specialCharacters = "!@#$%^&*()";
+  
+  for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+   }
+
+}
+
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -10,7 +38,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
