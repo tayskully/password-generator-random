06b4fef38a3fb61e786dd85ae2b0e946176a96c1
Taylor <taysully94@gmail.com>
2023-08-24 09:04:05 -0400

    adds a little bit of chaotic experimenting that i have to fix later

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
 // Assignment code here
 
-
-//button click - starts the prompts 
+//button click - starts the prompts
 
 //asks password length (I choose a length of at least 8 characters and no more than 128 characters)
 
@@ -11,28 +10,70 @@
 
 //generates password and puts it in either prompt or on the screen
 
-var promptIssues 
+var passwordLength = function () {
+  var userLength = window.prompt(
+    "How many characters does your password need to be? (must be above 8 characters)"
+  );
+  console.log(userLength);
+  if (userLength >= 8 && userLength < 128) {
+    passwordPref();
+  } else if (userLength > 128) {
+    window.prompt(
+      "Sorry, the password must be more than 8 characters. Try Again"
+    );
+  } else !userLength;
+  return;
+};
 
+var passwordPref = function () {
+  var userPref = window.prompt(
+    "Does it need to include Upper Case Letters? y or n"
+  );
+};
+//var YesNo =
+//define yes as true and no as false boollean
+if (userPref === y) {
+  generatePassword();
+} else if (userPref === n) {
+}
+console.log(userPref);
 
 function generatePassword() {
   var numbers = "0123456789";
   var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-  var UpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var specialCharacters = "!@#$%^&*()";
-  
-  for (var i = 0; i <= passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * chars.length);
-    password += chars.substring(randomNumber, randomNumber +1);
-   }
 
+  var password = "";
+
+  for (var i = 0; i <= passwordLength; i++) {
+
+    var randomCharacter = Math.floor(Math.random() * specialCharacters.length);
+    password += specialCharacters.substring(randomCharacter, randomCharacter + 1);
+
+    var randomLower = Math.floor(Math.random() * lowerCase.length);
+    password += lowerCase.substring(randomLower, randomLower + 1);
+
+    var randomUpper = Math.floor(Math.random() * upperCase.length);
+    password += upperCase.substring(randomUpper, randomUpper + 1);
+
+    var randomNumber = Math.floor(Math.random() * numbers.length);
+    password += numbers.substring(randomNumber, randomNumber + 1);
+
+
+  }
 }
 
+password += upperCase.substring(randomNumber, randomNumber + 1);
 
+passwordLength();
 
-// Get references to the #generate element
+// // Get references to the #generate element
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -40,5 +81,6 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
+// // Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
