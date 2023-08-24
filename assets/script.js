24cb32594f8103880619616280d1d691cb5511d3
Taylor <taysully94@gmail.com>
2023-08-24 18:19:49 -0400

    adds a completed password length function, adds a completed passwrod preferences function, adds a completed generate password function

--- assets/script.js
+++ assets/script.js
@@ -1,76 +1,104 @@
-// Assignment code here
-
 //button click - starts the prompts
+// // // Get references to the #generate element
 
-//asks password length (I choose a length of at least 8 characters and no more than 128 characters)
+var generateBtn = document.querySelector("#generate");
 
-//asks character type preferences (I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters)
+var passwordChoices = []; //place to hold generated password elements
 
-//input should be validated and at least one character type should be selected
-
-//generates password and puts it in either prompt or on the screen
+// ask how long you want password
+//check if pw is bt 1 & 128 characters
+//if no ask again
+//if yes then put them in the !! list of chosen character !!
 
 var passwordLength = function () {
-  var userLength = window.prompt(
-    "How many characters does your password need to be? (must be above 8 characters)"
+  var userLength = parseInt(
+    window.prompt("How many characters does your password need to be?")
   );
   console.log(userLength);
   if (userLength >= 8 && userLength < 128) {
-    passwordPref();
+    console.log(userLength);
+    return userLength;
+  } else if (userLength < 8) {
+    window.prompt(
+      "Your password must be at least 8 characters, and no more than 128 characters.  Please try again."
+    );
+    passwordLength();
   } else if (userLength > 128) {
     window.prompt(
-      "Sorry, the password must be more than 8 characters. Try Again"
+      "Your password must be less than 128 characters.  Please try again."
     );
+    passwordLength();
   } else !userLength;
   return;
 };
 
+//ask if you we are using Upper Case?
+//are we using lower case characters?
+//are we using special characters?
+//are we using numerical characters?
 var passwordPref = function () {
-  var userPref = window.prompt(
-    "Does it need to include Upper Case Letters? y or n"
-  );
+  var userLowerCase = window.confirm("Are we using Lower Case characters?");
+  var userUpperCase = window.confirm("Are we using Upper Case characters?");
+  var userNumbers = window.confirm("Are we using Numbers?");
+  var userSpecicalChar = window.confirm("Are we using special characters?");
+  if (!userLowerCase || !userUpperCase || !userNumbers || !userSpecicalChar) {
+    window.alert("Hey! you done did it wrong!");
+    return passwordPref();
+  }
+  //didn't choose any, alert did something wrong, start function all over again
+
+  console.log(userLowerCase, userUpperCase, userNumbers, userSpecicalChar);
+  return {
+    userLowerCase, //these are key value pairs that re inside of an object
+    userUpperCase,
+    userNumbers,
+    userSpecicalChar,
+  };
 };
-//var YesNo =
-//define yes as true and no as false boollean
-if (userPref === y) {
-  generatePassword();
-} else if (userPref === n) {
-}
-console.log(userPref);
 
 function generatePassword() {
+  console.log(password); //testing button
+
+  //   //based on chosen length, select random characters from the chosen character set and put them in the password
+
   var numbers = "0123456789";
   var lowerCase = "abcdefghijklmnopqrstuvwxyz";
   var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var specialCharacters = "!@#$%^&*()";
+  var specialCharacters = "!@#$%^&*()!@#$%^&*()!@#$%^&*()"; //increased the probability of choosing a special character 
+  var length = passwordLength();
+  var pref = passwordPref();
+  var possibilities = ""; //bin with all the chosen option 
+  
+  if (pref.userLowerCase) { 
+    possibilities += lowerCase  //---this means possibilities equals possibilities plus lowerCase 
+  } 
+ if (pref.userUpperCase){
+  possibilities += upperCase
+ }
+ if (pref.userNumbers){
+  possibilities += numbers
+ }
+  if (pref.userSpecicalChar){
+    possibilities += specialCharacters
+  };
 
   var password = "";
 
-  for (var i = 0; i <= passwordLength; i++) {
-
-    var randomCharacter = Math.floor(Math.random() * specialCharacters.length);
-    password += specialCharacters.substring(randomCharacter, randomCharacter + 1);
-
-    var randomLower = Math.floor(Math.random() * lowerCase.length);
-    password += lowerCase.substring(randomLower, randomLower + 1);
-
-    var randomUpper = Math.floor(Math.random() * upperCase.length);
-    password += upperCase.substring(randomUpper, randomUpper + 1);
-
-    var randomNumber = Math.floor(Math.random() * numbers.length);
-    password += numbers.substring(randomNumber, randomNumber + 1);
-
+  for (var i = 0; i < length; i++) {
+    var randomNumber = Math.floor(Math.random() * possibilities.length);
+    password += possibilities[randomNumber]
+  
 
   }
+
+  return password;
 }
 
-password += upperCase.substring(randomNumber, randomNumber + 1);
+// //asks character type preferences (I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters)
 
-passwordLength();
+// //input should be validated and at least one character type should be selected
 
-// // Get references to the #generate element
-
-var generateBtn = document.querySelector("#generate");
+// //generates password and puts it in either prompt or on the screen
 
 // // Write password to the #password input
 
